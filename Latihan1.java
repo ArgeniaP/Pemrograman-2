ecfe0515d31d82bc0e743e348ba7ffbdcbfcd3ff
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2019-01-26 09:29:49 +0700

    membuat Latihan1.java

---
+++ Latihan1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Latihan1{
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Masukan nilai yang didapatkan oleh Mahasiswa (A/B/C/D/E):");
+    String nilai = s.next();
+    String ket = "";
+    switch (nilai) {
+      case "A":
+        ket = "Excellent!";
+        break;
+      case "B":
+        ket = "Good job!";
+        break;
+      case "C":
+        ket = "Study Harder!";
+        break;
+      default:
+        ket = "Sorry, you failed.";
+        break;
+    }
+    System.out.println("Comment: "+ket);
+
+    System.out.println("Masukan tiga nilai ujian anda untuk dihitung rata-ratanya:");
+    int nilai1 = s.nextInt();
+    int nilai2 = s.nextInt();
+    int nilai3 = s.nextInt();
+
+    double eva = (double)(nilai1+nilai2+nilai3)/3;
+    if (eva >= 60) {
+      System.out.println("Nilai rata-rata ujian anda adalah "+eva+", selamat ya :D");
+    } else {
+      System.out.println("Nilai rata-rata ujian anda adalah "+eva+", sangat disayangkan :(");
+    }
+  }
+}
