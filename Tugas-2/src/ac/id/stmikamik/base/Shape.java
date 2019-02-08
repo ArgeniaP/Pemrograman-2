b46a6e606d67552cc6b53bbee8394c60706aed3e
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2019-02-08 21:41:19 +0700

    Membuat Shape.java

---
+++ Tugas-2/src/ac/id/stmikamik/base/Shape.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.id.stmikamik.base;
+
+/**
+ *
+ * @author user
+ */
+public abstract class Shape {
+    
+    public abstract long getArea();
+    
+    public abstract long getName();
+    
+}
