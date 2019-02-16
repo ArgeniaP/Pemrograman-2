e3334d59e50db4c15475a7d40c555c4c55a26880
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2019-02-16 14:01:07 +0700

    Mengimplementasikan Method pada Main.java

--- Pra UTS/Soal3/src/perusahaan/data/Main.java
+++ Pra UTS/Soal3/src/perusahaan/data/Main.java
@@ -16,6 +16,23 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Employee boss = new Manajer(250000, "Bayu Pamungkas", "Meleber", 3000000);
+        Employee staff1 = new Staff("Kiki Permata", "Cibeunying Kidul", 2750000);
+        Employee staff2 = new Staff("Panji Sandi", "Kiaracondong", 2700000);
+        
+        System.out.println("Nama Manajer: " + boss.getName());
+        System.out.println("Alamat Manajer: " + boss.getAddress());
+        System.out.println("Salary Manajer: " + boss.getSalary());
+        System.out.println("===================================");
+        
+        System.out.println("Nama Staff: " + staff1.getName());
+        System.out.println("Alamat Staff: " + staff1.getAddress());
+        System.out.println("Salary Staff: " + staff1.getSalary());
+        System.out.println("===================================");
+        
+        System.out.println("Nama Staff: " + staff2.getName());
+        System.out.println("Alamat Staff: " + staff2.getAddress());
+        System.out.println("Salary Staff: " + staff2.getSalary());
     }
     
 }
