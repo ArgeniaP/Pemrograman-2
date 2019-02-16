b089e5f7d0a6fe2b4e09bb3deb577b55b67e251a
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2019-02-16 12:16:21 +0700

    Menyelesaikan Soal2

--- Pra UTS/Soal2/src/terbesar/data/Main.java
+++ Pra UTS/Soal2/src/terbesar/data/Main.java
@@ -16,6 +16,24 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[] data = new int[args.length];
+        for(int i = 0; i < args.length; i++){
+            data[i] = Integer.parseInt(args[i]);
+        }
+        
+        for(int i = 1; i < data.length; i++){
+            int key = data[i];
+            int j = i-1;
+            while(j >= 0 && data[j] > key){
+                data[j+1] = data[j];
+                j = j-1;
+            }
+            data[j+1] = key;
+        }
+        
+        for(int i = 7; i > 5; i--){
+            System.out.print(data[i] + " ");
+        }
     }
     
 }
