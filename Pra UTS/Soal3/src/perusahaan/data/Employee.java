c8186e413bbab8f223fae1d323ecb1da4dabaacb
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2019-02-16 13:41:04 +0700

    Membuat method constructor kosong dan getter and setter variable class

--- Pra UTS/Soal3/src/perusahaan/data/Employee.java
+++ Pra UTS/Soal3/src/perusahaan/data/Employee.java
@@ -10,5 +10,34 @@ package perusahaan.data;
  * @author user
  */
 public class Employee {
-    
+    private String name;
+    private String address;
+    private long salary;
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
 }
