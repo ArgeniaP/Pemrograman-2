4cb68dc2c30e7e96eb91d6eaf8860557317a404e
Ilham Taufik Hidayat <ilham.t.hidayat@gmail.com>
2019-02-16 11:06:54 +0700

    Penerapan di Main.java kondisi kedua lingkungan

--- Pra UTS/Soal1/src/lingkaran/koordinat/Main.java
+++ Pra UTS/Soal1/src/lingkaran/koordinat/Main.java
@@ -21,6 +21,31 @@ public class Main {
         Lingkaran c1 = new Lingkaran();
         Lingkaran c2 = new Lingkaran();
         Scanner s = new Scanner(System.in);
+        
+        System.out.println("Silahkan masukan data dari lingkaran C1, dengan format (x,y,r):");
+        String[] raw1 = s.next().split(",", 3);
+        c1.setX(Integer.parseInt(raw1[0]));
+        c1.setY(Integer.parseInt(raw1[1]));
+        c1.setR(Integer.parseInt(raw1[2]));
+        
+        System.out.println("Silahkan masukan data dari Lingkaran C2, dengan format (x,y,r):");
+        String[] raw2 = s.next().split(",", 3);
+        c2.setX(Integer.parseInt(raw2[0]));
+        c2.setY(Integer.parseInt(raw2[1]));
+        c2.setR(Integer.parseInt(raw2[2]));
+        
+        int jarak = (int) Math.sqrt(Math.pow((c1.getX() - c2.getX()), 2) + Math.pow((c1.getY()-c2.getY()), 2));
+        
+        if(jarak >= (c1.getR()+c2.getR())){
+            System.out.println("C1 dan C2 Saling Lepas");
+        } else if(jarak < (c1.getR()+c2.getR())){
+            if((c1.getR() < c2.getR()) && (((c1.getX()+c1.getR()<c2.getX()+c2.getR())||(c1.getX()-c1.getR()>c2.getX()-c2.getR()) && (c1.getY()+c1.getR()<c2.getY()+c2.getR())||(c1.getY()-c1.getR()>c2.getY()-c2.getR())))){
+                System.out.println("C1 ada di dalam C2");
+            } else if((c1.getR() > c2.getR()) && (((c1.getX()+c1.getR()>c2.getX()+c2.getR())||(c1.getX()-c1.getR()<c2.getX()-c2.getR()) && (c1.getY()+c1.getR()>c2.getY()+c2.getR())||(c1.getY()-c1.getR()<c2.getY()-c2.getR())))){
+                System.out.println("C2 ada di dalam C1");
+            } else {
+                System.out.println("C1 dan C2 saling beririsan");
+            }
+        }
     }
-    
 }
